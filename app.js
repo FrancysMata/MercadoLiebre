78eedeb2908c38d9ea52f770bb00c73d197f5938
francys Mata <francysmata@gmail.com>
2022-01-31 20:09:32 +0100

    proyecto en nnube

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require ('express');
+const path = require ('path');
+const app = express ();
+
+app.get ('/', (req, res)=>{
+    res.send ('Ya entré')
+})
+
+app.get ('/mipagina', (req, res)=> {
+    res.sendFile(path.resolve(__dirname, './views/index.html')
+)});
+
+
+app.get('/register', function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/registro.html'))
+})
+
+
+
+
+ app.use (express.static (path.join (__dirname, './public')));
+
+ app.get('/register', function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/registro.html'))
+})
+
+//pagina login
+//usar localhost:3030/login
+app.get('/login', function(req, res) {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
+
+app.use(express.static(path.join(__dirname, './public')))
+app.use(express.static(path.join(__dirname, './views')))
+
+ app.listen (3030, ()=>
+ console.log ('Servidor Corriendo'))
+
+ app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+})
+
